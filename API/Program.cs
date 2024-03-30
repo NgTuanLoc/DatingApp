030e31d8bbc2e614e5647dc59133ab7e3a33321a
Nguyễn Tuấn Lộc <74365102+NgTuanLoc@users.noreply.github.com>
2024-03-30 11:02:45 +0700

    setup Angular client App

--- API/Program.cs
+++ API/Program.cs
@@ -11,11 +11,11 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 
