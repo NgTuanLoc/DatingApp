767b8e353efb9a13c2745882f0de78c205813545
Nguyễn Tuấn Lộc <74365102+NgTuanLoc@users.noreply.github.com>
2024-04-27 10:28:47 +0700

    setup Repository pattern and add AutoMapper

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,27 +1,35 @@
 using API.Data;
+using API.DTOs;
 using API.Entities;
+using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-// [Authorize]
-public class UsersController(DataContext context) : BaseController
+[Authorize]
+public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseController
 {
-    private readonly DataContext _context = context;
+    private readonly IMapper _mapper = mapper;
+    private readonly IUserRepository _userRepository = userRepository;
 
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(CancellationToken cancellationToken)
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
-        var users = await _context.Users.ToListAsync(cancellationToken);
-        return Ok(users);
+        // var users = await _userRepository.GetUsersAsync();
+        // var usersDto = _mapper.Map<IEnumerable<MemberDto>>(users);
+        var usersDto = await _userRepository.GetMembersAsync();
+        return Ok(usersDto);
     }
 
-    [HttpGet("{id}")]
-    public async Task<ActionResult<AppUser>> GetUserById(int id, CancellationToken cancellationToken)
+    [HttpGet("{username}")]
+    public async Task<ActionResult<AppUser>> GetUserByUsername(string username)
     {
-        var user = await _context.Users.FindAsync(id, cancellationToken);
-        return Ok(user);
+        // var user = await _userRepository.GetUserByUsernameAsync(username);
+        // var userDto = _mapper.Map<MemberDto>(user);
+        var userDto = await _userRepository.GetMemberByUsernameAsync(username);
+        return Ok(userDto);
     }
 }
